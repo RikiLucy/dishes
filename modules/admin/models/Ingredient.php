e30504cca31d4df414a1791dae0c7c68c527dd86
RikiLucy <riki.and.lucy@gmail.com>
2017-03-05 14:47:10 +0700

    модель и crud для ингредиентов

---
+++ modules/admin/models/Ingredient.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\modules\admin\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "ingredient".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $desc
+ * @property string $img
+ * @property string $status
+ */
+class Ingredient extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'ingredient';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['desc'], 'string'],
+            [['title', 'img'], 'string', 'max' => 255],
+            [['status'], 'string', 'max' => 1],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'desc' => 'Desc',
+            'img' => 'Img',
+            'status' => 'Status',
+        ];
+    }
+}
---
+++ modules/admin/views/ingredient/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\admin\models\Ingredient */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="ingredient-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'desc')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'img')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
